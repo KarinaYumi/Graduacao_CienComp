20e9b94a64f8d4517424bb420a7e9d573ee00181
Karina Yumi Ferreira Pinto <yumijapa2@gmail.com>
2024-10-30 20:44:49 -0300

    Create QuadradoNum.cpp

---
+++ 2Semestre/Atividade01/QuadradoNum.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int quadrado(int n){
+	int i;
+	int resultado = 0;
+	for(i = 0; i < n; i++){
+		resultado += (2*i + 1);
+	}
+	return resultado;
+}
+
+int main(){
+	int n;
+	printf("Digite um número positivo: ");
+	scanf("%d", &n);
+	
+	if(n > 0){
+		int resultado = quadrado(n);
+		printf("O quadrado de %d é: %d\n", n, resultado);
+		
+	}else{
+		printf("Digite um número POSITIVO: ");
+	}
+	
+	return 0;
+}
+
