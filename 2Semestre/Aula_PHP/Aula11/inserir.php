eab6e40778fde1292470d39fb9027a5615a17d66
Karina Yumi Ferreira Pinto <yumijapa2@gmail.com>
2024-11-26 20:23:52 -0300

    Create inserir.php

---
+++ 2Semestre/Aula_PHP/Aula11/inserir.php
@@ -0,0 +1,33 @@
+
+<html>
+	<head>
+	</head>
+		
+	<body>
+		
+
+		<form action = "inserir_proc.php" method = "post">
+			Nome:
+			<input type = "text" name = "nome_usuario"> <br/>
+				
+			RA (somente numeros):
+			<input type = "text" name = "ra"> <br/>
+				
+			Data de nascimento:
+			<input type = "date" name = "data_nascimento" value = "2023-11-06" min "1900-01-01" max = "2023-12-31"> <br/>
+				
+			Usuario:
+			<input type = "text" name = "usuario"> <br/>
+				
+			senha:
+			<input type = "password" name = "senha"> <br/>
+				
+			Email:
+			<input type = "email" name = "email"> <br/>
+			<input type = "submit" value = "Enviar">
+		</form>
+		
+		
+		
+	</body>
+</html>
